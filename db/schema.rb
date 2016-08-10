95cf2e45ebcfd31e850c0ea14115d461bf5dceff
hadthesha <shahadadwani@gmail.com>
2016-08-10 17:59:52 +0000

    Databases added, migrations created, tables modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160810173614) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string   "name"
+    t.string   "adress"
+    t.string   "price_range"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "restaurants_tags", force: :cascade do |t|
+    t.integer "restaurant_id"
+    t.integer "tag_id"
+  end
+
+  create_table "restaurants_users", force: :cascade do |t|
+    t.integer "restaurant_id"
+    t.integer "user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
