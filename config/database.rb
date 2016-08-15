67a367112e40c5987e0f3dfa0d62412f573af864
Sandra Tsai <sandraytsai@gmail.com>
2016-08-15 21:45:50 +0000

    new repo

--- config/database.rb
+++ config/database.rb
@@ -5,15 +5,11 @@ end
 
 configure :development, :test do
   set :database, {
-    'development' => {
-      'adapter' => 'sqlite3',
-      'database' => APP_ROOT.join('db', 'development.sqlite3')
-    },
-    'test' => {
-      'adapter' => 'sqlite3',
-      'database' => APP_ROOT.join('db', 'test.sqlite3')
+      'adapter': 'sqlite3',
+      'database': 'db/db.sqlite3'
     }
-  }
+  else
+    set :database, ENV['DATABASE_URL']
 end
 
 configure :production do
