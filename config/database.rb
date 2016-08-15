0d3ce4c48278b754bc05bda1c9931ceab5f86a79
Sandra Tsai <sandraytsai@gmail.com>
2016-08-15 22:03:38 +0000

    prepare for production

--- config/database.rb
+++ config/database.rb
@@ -4,10 +4,11 @@ configure :development do
 end
 
 configure :development, :test do
-  set :database, {
-      'adapter': 'sqlite3',
-      'database': 'db/db.sqlite3'
-    }
+  if development? 
+    set :database, {
+        'adapter': 'sqlite3',
+        'database': 'db/db.sqlite3'
+      }
   else
     set :database, ENV['DATABASE_URL']
 end
